54b14100746743c1a1f70934a49d80ffe75bdec1
Danito-gif <init.defaultBranch>
2026-04-28 00:37:58 +0200

    Corregido CI/CD Terraform con endpoint localstack

--- main.tf
+++ main.tf
@@ -13,16 +13,17 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localstack:4566"
+    ssm = "http://localstack:4566"
   }
 }
 
-resource "aws_s3_bucket" "bucket_ci" {
-  bucket = "bucket-cicd-danito"
+resource "aws_ssm_parameter" "parametro_ci" {
+  name  = "/danito/practica"
+  type  = "String"
+  value = "terraform-cicd-ok"
 }
