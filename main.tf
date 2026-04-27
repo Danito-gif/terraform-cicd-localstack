8fb263b0c5b8bafe960bfbde5af03bef7cf48ca8
Danito-gif <init.defaultBranch>
2026-04-28 00:32:35 +0200

    Corregido CI/CD Terraform con endpoint localstack

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.67"
     }
   }
 }
@@ -13,14 +13,13 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  s3_use_path_style           = true
+  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    s3  = "http://localstack:4566"
-    sts = "http://localstack:4566"
+    s3 = "http://localstack:4566"
   }
 }
 
