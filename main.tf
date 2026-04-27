62d30ddfb2d6bd6f3782fe1ba3615cd551adb00f
Danito-gif <init.defaultBranch>
2026-04-28 00:24:29 +0200

    Corregido CI/CD Terraform con endpoint localstack

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3  = "http://host.docker.internal:4566"
-    sts = "http://host.docker.internal:4566"
+    s3  = "http://localstack:4566"
+    sts = "http://localstack:4566"
   }
 }
 
